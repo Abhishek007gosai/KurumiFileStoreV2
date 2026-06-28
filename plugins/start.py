6cecb83ce80fbd534888dd03d31ab34fbcab614f
Abhishek007gosai <99321716+Abhishek007gosai@users.noreply.github.com>
2026-06-29 02:16:08 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -194,20 +194,10 @@ async def not_joined(client: Client, message: Message):
                                 expire_date=datetime.utcnow() + timedelta(seconds=FSUB_LINK_EXPIRY) if FSUB_LINK_EXPIRY else None)
                             link = invite.invite_link
 
-                    buttons.append([
-                        InlineKeyboardButton("• 𝙹𝙾𝙸𝙽 𝙲𝙷𝘼𝙉𝙉𝙀𝙇 •", url=link)
-                    ])
-
-                    buttons.append([
-                        InlineKeyboardButton(
-                            "• ᴍᴏʀᴇ ᴄʜᴀɴɴᴇʟs •",
-                            url="https://t.me/+HUIqsxBkZtxhNTA1"
-                        )
-                    ])
-
+                    buttons.append([InlineKeyboardButton(text=name, url=link)])
                     count += 1
                     await temp.edit(f"<b>{'! ' * count}</b>")
-                    
+
                 except Exception as e:
                     print(f"Error with chat {chat_id}: {e}")
                     return await temp.edit(
