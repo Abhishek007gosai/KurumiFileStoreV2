792779976274da3bcc8be319755c96952684dfd0
Abhishek007gosai <99321716+Abhishek007gosai@users.noreply.github.com>
2026-07-20 02:40:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -23,25 +23,17 @@ WORKERS = int(os.getenv("WORKERS", "5"))
 
 # MongoDB
 DB_URI = os.getenv("DB_URI", "")
-DB_NAME = os.getenv("DB_NAME", "cluster0")
-
+DB_NAME = "cluster0"
+# Force Subscribe
+FSUBS = [[-1002369123167, True, 5]] 
 # Channels
 DB_CHANNEL =  -1002497924209
-# Force Subscribe
-FSUB_CHANNEL = int(os.getenv("FSUB_CHANNEL", "0"))
-FSUB_REQUEST = os.getenv("FSUB_REQUEST", "True").lower() == "true"
-FSUB_TIMER = int(os.getenv("FSUB_TIMER", "10"))
-FSUBS = [[FSUB_CHANNEL, FSUB_REQUEST, FSUB_TIMER]]
 
 # Auto Delete
-AUTO_DEL = int(os.getenv("AUTO_DEL", "300"))
+AUTO_DEL = "300"
 
 # Admins
-ADMINS = [
-    int(x)
-    for x in os.getenv("ADMINS", "").split(",")
-    if x.strip()
-]
+ADMINS = [8771195193]
 
 # Bot Settings
 DISABLE_BTN = os.getenv("DISABLE_BTN", "True").lower() == "true"
@@ -49,16 +41,15 @@ PROTECT = os.getenv("PROTECT", "False").lower() == "true"
 
 # Messages
 MESSAGES = {
-    "START": os.getenv("START_MESSAGE", ""),
-    "FSUB": os.getenv("FSUB_MESSAGE", ""),
-    "ABOUT": os.getenv("ABOUT_MESSAGE", ""),
-    "REPLY": os.getenv("REPLY_MESSAGE", "Wrong Command"),
-    "SHORT_MSG": os.getenv("SHORT_MESSAGE", ""),
-    "START_PHOTO": os.getenv("START_PHOTO", ""),
-    "FSUB_PHOTO": os.getenv("FSUB_PHOTO", ""),
-    "SHORT_PIC": os.getenv("SHORT_PIC", ""),
-    "SHORT": os.getenv("SHORT", ""),
-}
+"START": "<b>ʜᴇʏ ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ ᴄᴏᴍᴍᴜɴɪᴛʏ ɪғ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ sᴜᴘᴘᴏʀᴛ ᴏᴜʀ ᴄᴏᴍᴍᴜɴɪᴛʏ ʏᴏᴜ ᴄᴀɴ ᴅᴏ sᴏ ʙʏ sᴜʙsᴄʀɪʙɪɴɢ ᴛᴏ ᴏᴜʀ ᴄʜᴀɴɴᴇʟ\nᴛʜᴀɴᴋs ғᴏʀ ʏᴏᴜʀ sᴜᴘᴘᴏʀᴛ</b>",
+"FSUB": "<b><blockquote>ʜᴇʟʟᴏ ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ <a href='https://t.me/Ecchi_Dex'>ᴇᴄᴄʜɪ ᴅᴇx</a></blockquote>ʏᴏᴜ ɴᴇᴇᴅ ᴛᴏ ᴊᴏɪɴ ɪɴ ᴍʏ ᴄʜᴀɴɴᴇʟ/ɢʀᴏᴜᴘ ғɪʀsᴛ, ᴘʟᴇᴀsᴇ sᴜʙsᴄʀɪʙᴇ ᴛᴏ ᴏᴜʀ ᴄʜᴀɴɴᴇʟs ᴛʜʀᴏᴜɢʜ ᴛʜᴇ ʙᴜᴛᴛᴏɴs ʙᴇʟᴏᴡ ᴀɴᴅ sᴛᴀʀᴛ ʙᴏᴛ ᴀɢᴀɪɴ<blockquote>ʜᴏᴡ ᴛᴏ ᴜsᴇ ʙᴏᴛ <a href=https://t.me/NexusTutorial/6>ᴛᴜᴛᴏʀɪᴀʟ ᴄʟɪᴄᴋ ʜᴇʀᴇ</a></blockquote></b>",
+"ABOUT": "<b>ʜᴇʏ ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ ᴏᴜʀ ᴄᴏᴍᴍᴜɴɪᴛʏ ɪғ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ sᴜᴘᴘᴏʀᴛ ᴏᴜʀ ᴄᴏᴍᴍᴜɴɪᴛʏ ʏᴏᴜ ᴄᴀɴ ᴅᴏ sᴏ ʙʏ sᴜʙsᴄʀɪʙɪɴɢ ᴛᴏ ᴏᴜʀ ᴄʜᴀɴɴᴇʟ ᴛʜᴀɴᴋs Fᴏʀ ʏᴏᴜʀ sᴜᴘᴘᴏʀᴛ\n❏ ʙᴏᴛ ᴄᴏᴍᴍᴀɴᴅs\n├/start : sᴛᴀʀᴛ ᴛʜᴇ ʙᴏᴛ\nsɪᴍᴘʟʏ ᴄʟɪᴄᴋ ᴏɴ ʟɪɴᴋ ᴀɴᴅ sᴛᴀʀᴛ ᴛʜᴇ ʙᴏᴛ ᴊᴏɪɴ ʙᴏᴛʜ ᴄʜᴀɴɴᴇʟs ᴀɴᴅ ᴛʀʏ ᴀɢᴀɪɴ ᴛʜᴀᴛs ɪᴛ.</b>",
+"REPLY": "<b>ᴡʀᴏɴɢ ᴄᴏᴍᴍᴀɴᴅ</b>",
+"SHORT_MSG": "<b><blockquote>ʏᴏᴜʀ ᴀᴅs ᴛᴏᴋᴇɴ ɪs ᴇxᴘɪʀᴇᴅ ᴘʟᴇᴀsᴇ ᴠᴇʀɪғʏ ᴛᴏ ʀᴇɢᴀɪɴ ᴀᴄᴄᴇss ᴛᴏ ᴛʜᴇ ʙᴏᴛs</blockquote>ᴡʜᴀᴛ ɪs ᴛʜᴇ ᴛᴏᴋᴇɴ?ᴛʜɪs ɪs ᴀɴ ᴀᴅs ᴛᴏᴋᴇɴ. ᴘᴀssɪɴɢ ᴏɴᴇ ᴀᴅ ᴀʟʟᴏᴡs ʏᴏᴜ ᴛᴏ ᴜsᴇ ᴛʜᴇ ᴏᴜʀ ʙᴏᴛs</b>",
+"START_PHOTO": "https://i.ibb.co/0R9k9x4M/tmpbtpr7q0.jpg",
+"FSUB_PHOTO": "https://i.ibb.co/sdYHCnBC/tmp9peum4mg.jpg",
+"SHORT_PIC": "https://i.ibb.co/sdYHCnBC/tmp9peum4mg.jpg",
+"SHORT": "https://i.ibb.co/sdYHCnBC/tmp9peum4mg.jpg",}
 
 
 def LOGGER(name: str, client_name: str) -> logging.Logger:
